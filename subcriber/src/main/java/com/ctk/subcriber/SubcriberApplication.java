76af08ad572776c455fb4c2fc27d48bd0ddc3846
Tuan Kiet Cao <136156202+emkietthucode@users.noreply.github.com>
2025-08-11 19:24:03 +0700

    Add files via upload

---
+++ subcriber/src/main/java/com/ctk/subcriber/SubcriberApplication.java
@@ -0,0 +1,12 @@
+package com.ctk.subcriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubcriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubcriberApplication.class, args);
+		}
+}
